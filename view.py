ad828b5261c13964c0ea22b1f0070170e14e9da4
daminiborude <95354079+daminiborude@users.noreply.github.com>
2024-04-02 13:32:44 +0530

    Update view.py

--- view.py
+++ view.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
 from .models import Client, Project
 from .serializers import ClientSerializer, ProjectSerializer
 
@@ -9,3 +10,14 @@ class ClientViewSet(viewsets.ModelViewSet):
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Require authentication to access
+
+    def list(self, request, *args, **kwargs):
+        user_projects = Project.objects.filter(users=request.user)
+        serializer = self.get_serializer(user_projects, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        # Add logged-in user to the project's users when creating a project
+        request.data['users'] = [request.user.id]
+        return super().create(request, *args, **kwargs)
